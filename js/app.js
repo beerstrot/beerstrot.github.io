0ebc880677be3be419f7b4508cfac2b70d2e5bcf
beerstrot <beerstrot@gmail.com>
2015-02-19 08:53:03 +0100

    add google analytics

--- js/app.js
+++ js/app.js
@@ -58,3 +58,12 @@ $(document).foundation();
 	w.addEventListener( "devicemotion", checkTilt, false );
 
 })( this );
+
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-59917673-1', 'auto');
+  ga('send', 'pageview');
+
