29c26ca759749505cac23f3b891a98bb810b3dcf
Andrea Tagliazucchi <andrea@MacBook-Pro.local>
2016-12-26 15:29:08 +0100

    fix

--- js/deeplinking.js
+++ js/deeplinking.js
@@ -19,22 +19,20 @@ function getMobileOperatingSystem() {
 }
 
 function onLoad() {
-    var urlLink1 = "https://www.instagram.com/beerstrot";
-    var urlLink2 = "https://www.twitter.com/beerstrot";
-    var urlLink3 = "https://www.facebook.com/cavecchiabeerstrot";
-    switch (getMobileOperatingSystem()) {
-        case 'Android':
-            urlLink1 = "instagram://user?username=beerstrot";
-            urlLink2 = "twitter://user?username=beerstrot";
-            urlLink3 = "fb://profile/397632563730269";
-            break;
-        case 'iOS':
-            urlLink1 = "instagram://user?username=beerstrot";
-            urlLink2 = "twitter://user?username=beerstrot";
-            urlLink3 = "fb://profile/397632563730269";
-            break;
-        default:
-            break;
+    var urlLink1 = "https://www.facebook.com/cavecchiabeerstrot";
+        var urlLink2 = "https://www.instagram.com/beerstrot";
+
+        switch (getMobileOperatingSystem()) {
+            case 'Android':
+                urlLink2 = "instagram://user?username=beerstrot";
+                urlLink1 = "fb://page/1073741829";
+                break;
+            case 'iOS':
+                urlLink2 = "instagram://user?username=beerstrot";
+                urlLink1 = "fb://page/1073741829";
+                break;
+            default:
+                break;
     }
     document.getElementById('yourLink1').setAttribute('href', urlLink1);
     document.getElementById('yourLink2').setAttribute('href', urlLink2);
