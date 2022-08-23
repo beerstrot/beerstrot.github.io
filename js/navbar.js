e7816479472a903a091a8510a67adcf98d7924d6
Andrea Tagliazucchi <andreatagliazucchi@gmail.com>
2022-08-23 12:38:35 +0200

    test new menu

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,13 @@
+//hamburger menu
+
 const toggle = document.querySelector(".toggle");
-const menu = document.querySelector(".top-bar");
+const menu = document.querySelector(".menu");
 const items = document.querySelectorAll(".item");
 
 /* Toggle mobile menu */
 function toggleMenu() {
-  if (top-bar.classList.contains("active")) {
-    top-bar.classList.remove("active");
+  if (menu.classList.contains("active")) {
+    menu.classList.remove("active");
     toggle.querySelector("a").innerHTML = "<i class='fas fa-bars'></i>";
   } else {
     menu.classList.add("active");
@@ -27,10 +29,14 @@ function toggleItem() {
 
 /* Close Submenu From Anywhere */
 function closeSubmenu(e) {
-  let isClickInside = menu.contains(e.target);
+  if (menu.querySelector(".submenu-active")) {
+    let isClickInside = menu
+      .querySelector(".submenu-active")
+      .contains(e.target);
 
-  if (!isClickInside && menu.querySelector(".submenu-active")) {
-    menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    if (!isClickInside && menu.querySelector(".submenu-active")) {
+      menu.querySelector(".submenu-active").classList.remove("submenu-active");
+    }
   }
 }
 /* Event Listeners */
@@ -42,6 +48,4 @@ for (let item of items) {
   item.addEventListener("keypress", toggleItem, false);
 }
 document.addEventListener("click", closeSubmenu, false);
-
-
 
